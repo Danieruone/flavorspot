f77889f7afb3ce860447024664d22ce6d51dff4a
Daniel <danieruone@gmail.com>
2025-02-23 17:23:27 -0500

    Add navbar

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between px-4 md:px-[30%] py-4 shadow-md w-full">
+      <h1 className="text-[1.8rem] font-bold text-[#F97316]">FlavorSpot</h1>
+      <h1>Profile</h1>
+    </div>
+  );
+};
