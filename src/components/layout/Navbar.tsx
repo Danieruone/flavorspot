c6aa615ce97859087076535b9f4f275df317b09d
Daniel Mendoza <danieruone@gmail.com>
2025-02-23 19:29:17 -0500

    Change profile icon

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+// icons
+import { FaUserAlt } from "react-icons/fa";
+
 export const Navbar = () => {
   return (
     <div className="fixed top-0 z-10 flex items-center justify-between px-4 md:px-[30%] py-4 shadow-md w-full bg-white">
       <h1 className="text-[1.8rem] font-bold text-[#F97316]">FlavorSpot</h1>
-      <h1>Profile</h1>
+      <div className="flex items-center hover:underline cursor-pointer">
+        <FaUserAlt size={20} />
+        <h1 className="text-sm ml-2">Profile</h1>
+      </div>
     </div>
   );
 };
