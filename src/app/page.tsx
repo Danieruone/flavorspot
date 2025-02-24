6c932466a4d926117d2df1642e0ed3eb2045c4d4
Daniel Mendoza <danieruone@gmail.com>
2025-02-23 19:27:44 -0500

    Fix navbar in the top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Feed } from "@/components/sections/Feed";
 
 export default function Home() {
   return (
-    <div className="bg-[#FCFAF8]">
+    <div className="bg-[#FCFAF8] pt-16">
       <Navbar />
       <div className="px-4 md:px-[30%] py-10">
         <Feed />
