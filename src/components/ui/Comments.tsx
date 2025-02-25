40e3d05a72de2254ac4cd54a54f0a5d126354f17
Daniel Mendoza <danieruone@gmail.com>
2025-02-24 20:49:46 -0500

    Add comment component

--- src/components/ui/Comments.tsx
+++ src/components/ui/Comments.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { ProfileName } from "./ProfileName";
+
+// components
+import { Comment } from "./Comment";
 
 export const Comments = () => {
   return (
     <div className="p-4">
-      <div className="mt-3">
-        <ProfileName name="Angie" lastName="Barreto" />
-        <p className="text-sm">Uyy que rica hambuguecha 😍</p>
-      </div>
+      <Comment />
+      <Comment />
+      <Comment />
       <div className="mt-3 flex justify-center hover:underline cursor-pointer">
         <p className="text-sm text-gray-400">View all comments</p>
       </div>
